dfb26bbaa2d23cbd8a2f95eaa2821d2fa705d364
Kimseonbeen <ksb9242@gmail.com>
2023-02-15 13:11:55 +0900

    1. react init (useEffect)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []); // 코드가 한 번만 실행 하도록 보호 해줌
+
+  useEffect(() => {
+    console.log("I run when 'keyword' chnages.");
+  }, [keyword]); // 'keyword'가 변화할 때 코드를 실행할 거라고 react.js에게 알려주는역할
+
+  useEffect(() => {
+    console.log("I run when 'counter' chnages.");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when keyword & conuter change");
+  }, [keyword, counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
