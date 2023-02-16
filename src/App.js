c0753e63463feca5f3ecab56fc0414c16aeafef5
Kimseonbeen <ksb9242@gmail.com>
2023-02-16 10:45:16 +0900

    1. coin tracker challenge -end

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
   const [usd, setUSD] = useState(0);
+  const [seleted, setSelected] = useState([
+    {
+      symbol: "",
+      price: "",
+    },
+  ]);
+  const { symbol, price } = seleted;
   const onChange = (evnet) => setUSD(evnet.target.value);
+  const handleSelect = (event) => {
+    setSelected({
+      symbol: event.target.value.split(":")[0],
+      price: event.target.value.split(":")[1],
+    });
+    setUSD(0);
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
         setLoading(false);
+        setSelected(json[0].symbol);
+        setSelected({
+          symbol: json[0].symbol,
+          price: json[0].quotes.USD.price,
+        });
       });
   }, []);
   return (
@@ -19,14 +38,19 @@ function App() {
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={handleSelect}>
           {coins.map((coin) => (
-            <option key={coin.id} value={coin.symbol}>
+            <option
+              key={coin.id}
+              value={loading ? null : coin.symbol + ":" + coin.quotes.USD.price}
+            >
               {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
+      <p>selected : {symbol}</p>
+      <p>price : {price}</p>
       <hr />
       <h2>USD TO Coin Convert</h2>
       <div>
@@ -37,7 +61,7 @@ function App() {
           placeholder="write to USD"
         />
       </div>
-      <input type="number" disabled="true" />
+      <input value={(1 / price) * usd} type="number" disabled={true} />
     </div>
   );
 }
